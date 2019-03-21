d8ec30a68faf4133ec2d5bb20ed4168d4c38df3e
Shailen Sheth <sheths@asbindia.org>
2019-03-21 13:55:55 +0530

    Version 0.14
    Began to Implement Password Checker

--- src/UserFunctions.java
+++ src/UserFunctions.java
@@ -1,12 +1,16 @@
 import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileReader;
+
 public class UserFunctions {
     Scanner sc = new Scanner(System.in);
     boolean validated = false;
+
     void SignUp()
     {
         boolean start = false;
@@ -140,8 +144,6 @@ public class UserFunctions {
         //Checking if this is a valid username
             if(fileNames.contains(UsernameValidation))
             {
-                System.out.println();
-                System.out.println("Thank you for Logging In.");
             }
             else
             {
@@ -160,7 +162,12 @@ public class UserFunctions {
                 }
 
             }
+            //Entering Password
+        System.out.println();
+        System.out.println("Password:");
+        String Password = sc.next();
+        //Verifying Password
         }
 
-
     }
+}
